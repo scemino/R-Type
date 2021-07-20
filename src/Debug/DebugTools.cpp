baa82da82c8177791c887a2bdef11a858d0a48f0
scemino <scemino74@gmail.com>
2021-07-20 21:46:18 +0200

    Fix issue when expanding entity node with same name

--- src/Debug/DebugTools.cpp
+++ src/Debug/DebugTools.cpp
@@ -4,34 +4,42 @@
 
 namespace DebugTools {
 void show(entt::registry &registry) {
-  registry.each([&registry](auto e) {
-    std::string name;
-    auto nc = registry.try_get<NameComponent>(e);
-    name = nc ? nc->name : "unknown";
-    if (ImGui::TreeNode(name.c_str())) {
-      auto pc = registry.try_get<PositionComponent>(e);
-      if (pc) {
-        auto pos = pc->getPosition();
-        if (ImGui::DragFloat2("Position", &pos.x)) {
-          pc->setPosition(pos);
+  if(ImGui::TreeNode("Entities")) {
+    registry.each([&registry](auto e) {
+      std::string name;
+      auto nc = registry.try_get<NameComponent>(e);
+      name = nc ? nc->name : "unknown";
+      ImGui::PushID(static_cast<int>(e));
+      if (ImGui::TreeNode(name.c_str())) {
+        auto pc = registry.try_get<PositionComponent>(e);
+        if (pc) {
+          auto pos = pc->getPosition();
+          if (ImGui::DragFloat2("Position", &pos.x)) {
+            pc->setPosition(pos);
+          }
         }
+        auto mc = registry.try_get<MotionComponent>(e);
+        if (mc) {
+          ImGui::DragFloat2("Velocity", &mc->velocity.x);
+        }
+        auto cc = registry.try_get<CollideComponent>(e);
+        if (cc) {
+          ImGui::DragFloat4("Hit box", &cc->hitbox.min.x);
+        }
+        auto ac = registry.try_get<AnimationComponent>(e);
+        if (ac) {
+          ImGui::TextDisabled("Animation: %s", ac->current.c_str());
+          ImGui::TextDisabled("Frame: %lu", ac->frameIndex);
+          ImGui::TextDisabled("Loop: %d", ac->loop);
+        }
+        ImGui::TreePop();
       }
-      auto mc = registry.try_get<MotionComponent>(e);
-      if (mc) {
-        ImGui::DragFloat2("Velocity", &mc->velocity.x);
-      }
-      auto cc = registry.try_get<CollideComponent>(e);
-      if (cc) {
-        ImGui::DragFloat4("Hit box", &cc->hitbox.min.x);
-      }
-      auto ac = registry.try_get<AnimationComponent>(e);
-      if (ac) {
-        ImGui::TextDisabled("Animation: %s", ac->current.c_str());
-        ImGui::TextDisabled("Frame: %lu", ac->frameIndex);
-        ImGui::TextDisabled("Loop: %d", ac->loop);
-      }
-      ImGui::TreePop();
-    }
-  });
+      ImGui::PopID();
+    });
+    ImGui::TreePop();
+  }
+
+  auto& io = ImGui::GetIO();
+  ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
 }
 }
